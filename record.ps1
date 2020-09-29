41b06e9a3a28c88f142ccbf6521537feab635568
Prancing-Pony <mrunderhill.bree@gmail.com>
2020-09-29 07:53:07 -0700

    rotate

--- record.ps1
+++ record.ps1
@@ -1,26 +1,4 @@
-﻿
-foreach($sentence in Get-Content -encoding UTF8 $args[0]) {
-    # Create a without invalid characters
-    $filename = $sentence;
-    [System.IO.Path]::GetInvalidFileNameChars() | % {$filename = $filename.replace($_,'_')};
-
-    # Record the sentence to filename.mp4
-    Write-Host "Recording: $line press q to finish";
-    C:\Users\Hanzhou\Downloads\ffmpeg.exe -y -loglevel quiet -f dshow -i video="DroidCam Source 3":audio="Microphone (DroidCam Virtual Audio)" "${filename}.mp4";
-
-    # Create a slowed version of the file "slow_$filename.mp4"
-    CreateSlowedFile "${filename}.mp4}";
-
-    # Play the slowed down audio 
-    Start-Job -ScriptBlock {PlaySlowedAudio "slow_${filename}.mp4";} | out-null;
-
-    Write-Host "Press any key once you hear the character below being pronounced";
-    
-    # Record the speech timing
-    RecordSpeechTiming $filename $line;
-}
-
-function RecordSpeechTiming {
+﻿function RecordSpeechTiming {
     Param (
         [string] [Parameter(Mandatory=$true)] $filename,
         [string] [Parameter(Mandatory=$true)] $sentence
@@ -28,12 +6,12 @@ function RecordSpeechTiming {
     # I'll assume normalized
     $stopwatch = New-Object System.Diagnostics.Stopwatch;
     $stopwatch.Start();
-    $charArray = $line.toCharArray();
+    $charArray = $sentence.toCharArray();
     $object = @{
         "sentence" = $sentence;
-        "timeScale" = 0.5;
+        "timeScale" = 0.25;
         "characters" = @();
-        "filename" = $filename;
+        "filename" = "${filename}.mp4";
     };
     foreach($char in $charArray) {
         if($char -cmatch '[\u4e00-\u9fff]'){
@@ -41,23 +19,81 @@ function RecordSpeechTiming {
             $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown') | out-null;
         }
         $timestamp = $stopwatch.Elapsed.TotalMilliseconds;
-        $timestamps += , @{'timestamp' = $timestamp; 'character' = $char};
+        $object['characters'] += , @{'timestamp' = $timestamp; 'character' = $char};
     }
-    ConvertTo-Json $timestamps | Set-Content "${filename}.json"
+    ConvertTo-Json $object | Set-Content -Encoding UTF8 "${filename}.json"
 };
 
-function PlaySlowedAudio {
-    Param (
-        [string] [Parameter(Mandatory=$true)] $videoFile
-    );
-        
-    C:\Users\Hanzhou\Downloads\ffplay.exe -loglevel quiet -autoexit -nodisp $slowedFile;
-};
+
 
 function CreateSlowedFile {
     Param (
-        [string] [Parameter(Mandatory=$true)] $videoFile
+        [string] [Parameter(Mandatory=$true)] $filename
     );
-    $slowedFile = "slow_${videoFile}";
-    ffmpeg -i $video File -loglevel quiet -filter:v "setpts=2*PTS" -filter:a "atempo=0.5" $slowedFile;
+    $slowedFile = "slow_${filename}.aac";
+    Write-Host "Creating slowed file $slowedFile";
+    ffmpeg.exe -i "${filename}.mp4" `
+        -y `
+        -loglevel error `
+        -vn `
+        -filter:a "atempo=0.5,atempo=0.5" `
+        "$slowedFile";
 }
+
+function RotateVideo {
+    Param (
+        [string] [Parameter(Mandatory=$true)] $filename
+    );
+
+}
+
+
+foreach($sentence in Get-Content -encoding UTF8 $args[0]) {
+    # Create a without invalid characters
+    $filename = $sentence;
+    [System.IO.Path]::GetInvalidFileNameChars() | % {$filename = $filename.replace($_,'_')};
+
+    # Record the sentence to filename.mp4
+    Write-Host "Recording: $sentence press q to finish";
+    ffmpeg.exe -y `
+        -loglevel error `
+        -f dshow ` `
+        -i video="DroidCam Source 3":audio="Microphone (DroidCam Virtual Audio)" `
+        "${filename}_90.mp4";
+
+    Write-Host "Rotating video 90 degrees";
+    ffmpeg.exe -y `
+        -loglevel error `
+        -i "${filename}_90.mp4" `
+        -vf "transpose=1" `
+        "${filename}.mp4" 
+
+
+    # Create a slowed version of the file "slow_$filename.mp4"
+    CreateSlowedFile $filename;
+
+    $slowFilename = "slow_${filename}.aac";
+    # Play the slowed down audio
+    $job = Start-Job `
+        -InputObject $slowFilename `
+        -Init ([ScriptBlock]::Create("Set-Location '$pwd'")) `
+        -ScriptBlock { 
+            [Console]::OutputEncoding = [Text.Encoding]::Utf8;
+            function PlaySlowedAudio {
+                Param (
+                    [string] [Parameter(Mandatory=$true)] $slowFilename
+                );
+                Write-Output "Playing $slowFilename";
+                ffplay.exe `
+                    -loglevel error `
+                    -autoexit `
+                    -nodisp $slowFilename;
+                Write-Output "Finished playing $slowFilename";
+            };
+            PlaySlowedAudio $input; 
+        };
+    Write-Host "Press any key once you hear the character below being pronounced";
+    # Record the speech timing
+    RecordSpeechTiming $filename $sentence;
+}
+
