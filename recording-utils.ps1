5e8c5f7fdf9173eebf0901b21b7307a0909907af
Prancing-Pony <mrunderhill.bree@gmail.com>
2020-10-28 21:33:19 -0700

    preview with sdl, still sux that it only appears once recording starts

--- recording-utils.ps1
+++ recording-utils.ps1
@@ -7,13 +7,12 @@
 }
 
 #http://jongurgul.com/blog/get-stringhash-get-filehash/ 
-Function Get-StringHash([String] $String,$HashName = "SHA1") 
-{ 
-$StringBuilder = New-Object System.Text.StringBuilder 
-[System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String))|%{ 
-[Void]$StringBuilder.Append($_.ToString("x2")) 
-} 
-$StringBuilder.ToString() 
+Function Get-StringHash([String] $String,$HashName = "SHA1") { 
+    $StringBuilder = New-Object System.Text.StringBuilder 
+    [System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String)) | % { 
+        [Void]$StringBuilder.Append($_.ToString("x2")) 
+    } 
+    $StringBuilder.ToString() 
 }
 
 function RecordSpeechTiming {
@@ -50,7 +49,6 @@ function CreateSlowedFile {
     Write-Host "Creating slowed file $slowedFile";
     ffmpeg.exe -i "${filename}.mp4" `
         -y `
-        -loglevel fatal `
         -vn `
         -filter:a "atempo=0.5" `
         "$slowedFile";
@@ -65,22 +63,41 @@ function RecordSentence {
 
     # Record the sentence to filename.mp4
     Write-Host "Recording: $sentence press q to finish";
-    ffmpeg.exe -y `
-        -loglevel fatal `
-        -f dshow ` `
-        -i video="DroidCam Source 3":audio="Microphone (DroidCam Virtual Audio)" `
-        "${filename}_90.mp4";
+    # TODO will this resolve the location with PATH?
+    Invoke-NativeCommand -FilePath ffmpeg.exe -ArgumentList @(
+        "-y",
+        "-f", "dshow",
+        "-loglevel", "fatal",  
+        "-i", "video=@device_pnp_\\?\usb#vid_07ca&pid_313a&mi_00#7&14881977&0&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\global:audio=@device_cm_{33D9A762-90C8-11D0-BD43-00A0C911CE86}\wave_{6EDC73A4-7FF7-4F94-957A-AB29B58F2895}",
+        "${filename}.mp4",
+        "-pix_fmt", "yuv420p",
+        "-window_x", "0",
+        "-window_y", "0",
+        "-f", "sdl", ":0"
+    ) | Receive-RawPipeline;
 
-    Write-Host "Rotating video 90 degrees";
+    <#ffmpeg.exe -y `
+        -f dshow `
+        -rtbufsize 100M `
+        -s 1920x1080 `
+        -r 30 `
+        -i video="@device_pnp_\\?\usb#vid_07ca&pid_313a&mi_00#7&14881977&0&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\global":audio="@device_cm_{33D9A762-90C8-11D0-BD43-00A0C911CE86}\wave_{6EDC73A4-7FF7-4F94-957A-AB29B58F2895}" `
+        -c:v libx264 `
+        -q 0 `
+        -f h264 – |
+        ffmpeg -f h264 -i – -an -c:v copy -f mp4 file.mp4 -an -c:v copy -f h264 pipe:play | ffplay -i pipe:play
+        "${filename}_90.mp4";#>
+
+    <# Write-Host "Rotating video 90 degrees";
     ffmpeg.exe -y `
         -loglevel fatal `
         -i "${filename}_90.mp4" `
         -vf "transpose=2" `
         "${filename}.mp4" 
-    Remove-Item "${filename}_90.mp4"
+    Remove-Item "${filename}_90.mp4"#>
 
 
-    # Create a slowed version of the file "slow_$filename.mp4"
+    # Create a slowed version of the file "slow_$filename.mp4"`
     CreateSlowedFile $filename;
 
     $slowFilename = "slow_${filename}.aac";
